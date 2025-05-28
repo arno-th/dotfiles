969053ac43a85f39d223cf858a76fc1e0ab13af7
Arno T <captaincoyote10@gmail.com>
2025-05-28 23:41:00 +0800

    Update polybar config

--- env/.config/polybar/launch.sh
+++ env/.config/polybar/launch.sh
@@ -6,7 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar i3_bar --config=$HOME/.config/polybar/config.ini &
+# Launch Polybar on each monitor, using default config location ~/.config/polybar/config
+for m in $(polybar --list-monitors | cut -d':' -f1); do
+    MONITOR=$m polybar i3_bar &
+done
 
 echo "Polybar launched..."
