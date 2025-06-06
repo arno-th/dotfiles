60ba00f37c4f9f51f132400ba016d1da523624f4
Arno T <captaincoyote10@gmail.com>
2025-06-06 17:00:01 +0800

    Add utils script

---
+++ env/.config/scripts/extend-monitors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Get connected monitors
+xrandr | grep " connected"
+xrandr --output "DP-0" --auto --primary
+xrandr --output "HDMI-0" --auto --right-of "DP-0"
+
+# Restart i3 to trickle changes
+i3-msg restart
